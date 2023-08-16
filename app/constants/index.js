ae0d9e27daf9bff2ceef28ddb177cf6fa4b46f19
RomanoBlockChain <romano.blockchain@gmail.com>
2023-08-16 22:54:01 +0700

    update

--- app/constants/index.js
+++ app/constants/index.js
@@ -1,3 +1,3 @@
 import { PublicKey } from "@solana/web3.js";
 
-export const TODO_PROGRAM_PUBKEY = new PublicKey("FHi9b593PHHVjcrvRBXjAmRd6JsHkWCoLmC8CHdkDUy3");
+export const TODO_PROGRAM_PUBKEY = new PublicKey("8qykv837hVgc2f7RCxEohnSPH8ncG2RT8atn4cTc5ijU");
